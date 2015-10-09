e4c9bc7646c1b6ea9f4131df387018575a467792
Jack Connor <jack.connor83@gmail.com>
2015-10-09 09:04:58 -0700

    challengemaker public/index/server file structure up, routing now all going through angular, and running on port 5555

---
+++ server.js
@@ -0,0 +1,24 @@
+var express        = require('express');
+var app            = express();
+var mongoose       = require('mongoose');
+var morgan         = require('mongoose');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+
+app.use(express.static(__dirname + '/public'));
+
+////setting views directory for express, though all routing will go through Angular
+
+app.set('views', __dirname + 'views/pages');
+app.set('view engine', 'ejs');
+
+app.get('*', function(req, res){
+  res.sendFile('./public/index.html')
+})
+
+////setting ports for local and remote, and listening
+app.set('port', (process.env.PORT || 5000))
+
+app.listen(app.get('port'), function(){
+  console.log("Server running smoother than Barry White");
+})
