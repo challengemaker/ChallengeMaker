50d1b74c737347527ccb99dc5f9772f4bb62d45d
Jack Connor <jack.connor83@gmail.com>
2015-10-20 22:29:53 -0700

    user signup page created

--- public/scripts/controllers/responseController.js
+++ public/scripts/controllers/responseController.js
@@ -26,9 +26,9 @@ angular.module('responseController', [])
     $('.submitDon').on("click", function(){
       if (window.localStorage.sessionUser && window.localStorage.sessionUser != "none") {
       window.location.hash = "#/"
-    } else if() {
-      window.location.hash = "#/signup"
-    }
+    } else {
+        window.location.hash = "#/signup"
+      }
     })
 
     $(".backToSite").on('click', function(){
