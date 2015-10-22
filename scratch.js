dc10a9d67427d7859c80d0a452f021c807df9e33
Jack Connor <jack.connor83@gmail.com>
2015-10-21 22:48:45 -0700

    added opacity fix

--- scratch.js
+++ scratch.js
@@ -20,4 +20,5 @@
 
 
 // db.challenges.insert({"title" : "Pie in the Face Challenge", "description" : "n the next 24 hours take a pie in the face or give $50 to Children’s Hospital Los Angeles and challenge four friends to do the same.", "videoUrl" : "https://www.youtube.com/embed/Aps6g1I5Dm4", "photo" : "http://s18.postimg.org/javhozzt5/Screen_Shot_2015_10_20_at_10_49_46_AM.png", "charityIcon" : "http://www.designeastoflabrea.org/wp-content/uploads/2014/10/FOLAR-lgo-HR_Navy.jpg", "createdBy" : [ { "name" : "Trevor", "userId" : 101010102 } ], "charity" : "Children’s Hospital Los Angeles", "responses" : [ ], "goal" : 10000, "minimumDonation" : 50, "totalRaised" : 6240 })
->>>>>>> 5e92032f132f521f826d41be6c2c5ee69503e0c0
+//
+// {"title" : "4Liter Challenge", "description" : "We challenge you to live one day in water poverty. For 24 hours, use just #4Liters of water for everything you do and change the way you think about water forever more info at 4liters.org. ", "videoUrl" : "https://www.youtube.com/embed/dNs8s2t1yWg", "photo" : "http://s13.postimg.org/60u6qnyuf/Screen_Shot_2015_10_21_at_4_39_23_PM.png", "createdBy" : [ { "name" : "Jason", "userId" : 101010103 } ], "charity" : "4Liters.org", "responses" : [ ], "goal" : 10000, "minimumDonation" : 50, "totalRaised" : 0 }
