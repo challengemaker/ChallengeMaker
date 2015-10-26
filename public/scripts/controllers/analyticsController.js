6c6ca3479562b9621212931b847792e38b9c0ba1
Jack Connor <jack.connor83@gmail.com>
2015-10-26 11:30:20 -0700

    password protected email list page

--- public/scripts/controllers/analyticsController.js
+++ public/scripts/controllers/analyticsController.js
@@ -16,5 +16,22 @@ var app = angular.module('analyticsController', [])
       console.log(self.allFriendsChallenges);
     })
 
+
+    /////check password
+    $('.pwChecker').on('click', function(){
+      var attempt = $('.pwAttempt').val();
+      console.log(attempt);
+      $http({
+        method: "POST"
+        ,url: "/api/password"
+        ,data: {password: attempt}
+      })
+      .then(function(data){
+        console.log(data);
+        self.passwordCorrect = data.data.valid;
+      })
+    })
+
+
     //////////end controller
   }
