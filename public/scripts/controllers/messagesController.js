18f54e32dc6506f754f6481bb4e350926b68a7c1
Jack Connor <jack.connor83@gmail.com>
2015-10-26 23:50:43 -0700

    last commit before code cleanup

---
+++ public/scripts/controllers/messagesController.js
@@ -0,0 +1,7 @@
+var app = angular.module('messagesController', [])
+
+  .controller('messagesCtrl', messagesCtrl)
+
+  function messagesCtrl(){
+    console.log('yo dog');
+  }
---
+++ public/scripts/controllers/paymentsController.js
@@ -0,0 +1,10 @@
+var app = angular.module('paymentsController', [])
+
+  .controller('paymentsCtrl', paymentsCtrl)
+
+  paymentsCtrl.$inject = ['$http'];
+
+  function paymentsCtrl($http){
+    console.log('lol paymnets');
+    
+  }
