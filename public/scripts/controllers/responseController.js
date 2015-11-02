c452a0f6ffd3dadf4b3c8f65694da2ff365c21f7
Jack Connor <jack.connor83@gmail.com>
2015-11-02 11:38:37 -0800

    fixed color issue on single challenge text

--- public/scripts/controllers/responseController.js
+++ public/scripts/controllers/responseController.js
@@ -26,7 +26,7 @@ angular.module('responseController', [])
         $http({
           method: "POST"
           ,url: "/api/sendemail/contact"
-          ,data: {sendeeEmail: "questions@challengemaker.com", subject: subject, text: message, email: email}
+          ,data: {sendeeEmail: "jason@challengemaker.com", subject: subject, text: message, email: email}
         })
         .then(function(data){
           console.log(data);
