2976ca985535cde6d7b7779a25c90f1a248d7e18
Jack Connor <jack.connor83@gmail.com>
2015-11-10 15:15:39 -0800

    delete responses now works in cms

--- public/scripts/controllers/cmsController.js
+++ public/scripts/controllers/cmsController.js
@@ -606,9 +606,10 @@ var app = angular.module('cmsController', [])
     function deleteResponse(x){
       $('#cmsDeleteResponse'+x).on('click', function(){
         var thisResponseId = self.allResponses[x]._id
+        console.log(thisResponseId)
         $http({
           method: "DELETE"
-          ,url: "/api/users/"+thisResponseId
+          ,url: "/api/responses/"+thisResponseId
         })
         .then(function(data){
           $('#cmsItemResponse'+x).animate({
@@ -652,58 +653,5 @@ var app = angular.module('cmsController', [])
         })
       })
     }
-
-
-    // $http({
-    //   method: "GET",
-    //   url: "/api/challengefriends"
-    // })
-    // .then(function(data){
-    //   console.log(data);
-    //   var allFriendsChallenges = data.data;
-    //   console.log(allFriendsChallenges);
-    //   for (var i = 0; i < allFriendsChallenges.length; i++) {
-    //     allFriendsChallenges[i].challengeUrlToSend = "https://challengemaker.herokuapp.com/#/youvebeenchallenged/"+allFriendsChallenges[i].challenge + "/" + allFriendsChallenges[i].friendVideoUrl.split('/')[4] + "/" + allFriendsChallenges[i].senderName.split(' ').join('-');
-    //     console.log(allFriendsChallenges[i]);
-    //   }
-    //   self.allFriendsChallenges = allFriendsChallenges
-    // })
-
-    /////check password
-    // $('.pwChecker').on('click', function(){
-    //   var attempt = $('.pwAttempt').val();
-    //   console.log(attempt);
-    //   $http({
-    //     method: "POST"
-    //     ,url: "/api/password"
-    //     ,data: {password: attempt}
-    //   })
-    //   .then(function(data){
-    //     console.log(data);
-    //     self.passwordCorrect = data.data.valid;
-    //     if(self.passwordCorrect){
-    //       $('.passwordBox').html('')
-    //     }
-    //   })
-    // })
-    // ///keypress event
-    // $('.pwAttempt').keypress(function(evt){
-    //   console.log(evt);
-    //   var attempt = $('.pwAttempt').val();
-    //   console.log(attempt);
-    //   $http({
-    //     method: "POST"
-    //     ,url: "/api/password"
-    //     ,data: {password: attempt}
-    //   })
-    //   .then(function(data){
-    //     console.log(data);
-    //     self.passwordCorrect = data.data.valid;
-    //     if(self.passwordCorrect){
-    //       $('.passwordBox').html('')
-    //     }
-    //   })
-    // })
-
     //////////end controller
   }
