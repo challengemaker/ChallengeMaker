0eb04bb51363426016d0f3c2456a5b5b7103e9e7
Jack Connor <jack.connor83@gmail.com>
2015-11-11 21:45:09 -0800

    fixed certain carousel frontend fixes

--- public/scripts/controllers/donationController.js
+++ public/scripts/controllers/donationController.js
@@ -81,6 +81,39 @@ angular.module('donationController', [])
     ///////////////end logic for forward and back button in the challenge path
     //////////////////////////////////////////////////////////////////////////
 
+    ///////must set the x-button, which resets back to the signle challenge page
+    ////////////////////////////////////////////////////////////////////////////
+    $('.xBar').on('click', function(){
+      var challengeName = window.location.hash.split("/")[2]
+      $('.xBar').css({
+        backgroundColor: '#D4D4D4'
+        ,color: 'white'
+      })
+      setTimeout(function(){
+        window.location.hash = "#/challenges/"+challengeName
+      }, 150)
+
+    })
+    function moveXButton(){
+      var windowSize = $(window).width()
+      if(windowSize > 515){
+        $('.xBar').css({
+          marginLeft: '95%'
+        })
+      } else if(windowSize > 0){
+        $('.xBar').css({
+          marginLeft: '82%'
+        })
+      }
+    }
+    moveXButton()
+    $(window).resize(function(){
+      moveXButton()
+    })
+    //////additional x-functionality so that it resizes at smaller sizes
+    /////////end click function
+    ///////////////////////////
+
   ///////end of the the Donation Controller
   /////////////////////////////////////////
   }
